bc63246590702093c38eddfbafd80b3bf38d4546
GuXiangLi <2645199099@qq.com>
2024-05-29 19:51:54 +0800

    First Commit.

---
+++ app/src/main/java/com/foodapp/android/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.foodapp.android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.foodapp.android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        
+    }
+}
